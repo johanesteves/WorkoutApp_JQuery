26dfd25cf5eba9592ddff6163ad5ebb72adc0eee
johanesteves <jsesteves91@gmail.com>
2018-01-02 23:58:47 -0500

    Removed extra comment

--- app/serializers/workout_serializer.rb
+++ app/serializers/workout_serializer.rb
@@ -1,4 +1,4 @@
 class WorkoutSerializer < ActiveModel::Serializer
   attributes :id, :name, :date
-  has_many :exercises
+  has_many :exercises #,through: :exercise_workouts, serializer: ExerciseWorkoutSerializer
 end
